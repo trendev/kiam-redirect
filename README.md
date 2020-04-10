5a7ec323221383b327aaa5c77d263d9937557e8e
Julien Sié <julien.sie@gmail.com>
2020-04-10 13:35:19 +0200

    URL set from env vars

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # kiam-redirect
 
 redirects all request from comptandye.fr to kiam.fr :smiley:
+
+### build 
+`docker build -t trendev/kiam-redirect .`
+
+### run
+`docker run -d -p 80:9000 -e URL="https://www.kiam.fr" trendev/kiam-redirect`
+
+### test
+`curl localhost`
+
+> `<a href="https://www.kiam.fr">Permanent Redirect</a>.`
--- main.go
+++ main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const baseURL = "https://kiam.fr"
+
+var url string
+
 func redirect(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://kiam.fr", http.StatusPermanentRedirect)
+	http.Redirect(w, r, url, http.StatusPermanentRedirect)
 }
 
 func main() {
+
+	if u := os.Getenv("URL"); u != "" {
+		url = u
+	} else {
+		url = baseURL
+	}
+
+	log.Printf("Server starting, listening on port :9000 and redirecting request to %v", url)
+
 	http.HandleFunc("/", redirect)
 	log.Fatal(http.ListenAndServe(":9000", nil))
 }
