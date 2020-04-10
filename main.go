a063fdd8481bef9819f3e9161488cd0f3102851d
Julien Sié <julien.sie@gmail.com>
2020-04-10 13:02:15 +0200

    init redirect with json response

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusPermanentRedirect)
+	w.Write([]byte(`{"message": "we moved to https://kiam.fr"}`))
+}
+
+func main() {
+	http.HandleFunc("/", redirect)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
